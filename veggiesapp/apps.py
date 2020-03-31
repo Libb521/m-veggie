0782f3de2d4175fd2e594cbe79c8392253c954ff
Libb521 <eotieno39@yahoo.com>
2020-03-31 12:20:03 +0300

    initial commit

---
+++ veggiesapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VeggiesappConfig(AppConfig):
+    name = 'veggiesapp'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringaschool/Desktop/Capstone/M-Veggies/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
